69bbb79cdcf6dd0094cc124de4b1b67fa6b3f7a9
agostinolandolfo21 <agostinolandolfo21@gmail.com>
2026-07-29 17:21:58 +0200

    Update Utente.java

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -1,17 +1,46 @@
 package model;
 
-import java.util.ArrayList;
-
-public class Utente {
-    private String login;
+public class UTENTE {
+    private String username;
     private String password;
+    private String nome;
+    private String cognome;
 
-    public Utente(String login, String password) {
-        this.login = login;
-        this.password = password;
+    /**
+     * Costruttore base dell'entità Utente.
+     */
+    public UTENTE() {
+        this.username = this.username;
+        this.password = this.password;
+        this.nome = this.nome;
+        this.cognome = this.cognome;
     }
 
-    public boolean login(String login, String password) {
-        return ( login.equals(this.login) && password.equals(this.password));
+    public UTENTE(String username, String password, String nome, String cognome, String codiceDocente) {
+    }
+
+    // --- METODI GETTER ---
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
     }
 }
