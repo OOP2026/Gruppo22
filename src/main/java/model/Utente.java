6bf7cf42a5442db5fe05fb372037f3a5592d8ac5
agostinolandolfo21 <agostinolandolfo21@gmail.com>
2026-07-29 19:44:47 +0200

    Fix Utente class: match filename, correct constructors, initialize fields, add codiceDocente field and accessors

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -1,22 +1,29 @@
 package model;
 
- public class Utente {
+public class Utente {
     private String username;
     private String password;
     private String nome;
     private String cognome;
+    private String codiceDocente;
 
     /**
      * Costruttore base dell'entità Utente.
      */
-    public UTENTE() {
-        this.username = this.username;
-        this.password = this.password;
-        this.nome = this.nome;
-        this.cognome = this.cognome;
+    public Utente() {
+        this.username = "";
+        this.password = "";
+        this.nome = "";
+        this.cognome = "";
+        this.codiceDocente = "";
     }
 
-    public UTENTE(String username, String password, String nome, String cognome, String codiceDocente) {
+    public Utente(String username, String password, String nome, String cognome, String codiceDocente) {
+        this.username = username;
+        this.password = password;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.codiceDocente = codiceDocente;
     }
 
     // --- METODI GETTER ---
@@ -36,11 +43,28 @@ package model;
         return cognome;
     }
 
+    public String getCodiceDocente() {
+        return codiceDocente;
+    }
+
+    // --- METODI SETTER ---
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
 
     public void setCognome(String cognome) {
         this.cognome = cognome;
+    }
+
+    public void setCodiceDocente(String codiceDocente) {
+        this.codiceDocente = codiceDocente;
     }
 }
