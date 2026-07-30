c9c9306143e1a92a951ae6cd5295d50b6c4bdbce
agostinolandolfo21 <agostinolandolfo21@gmail.com>
2026-07-30 14:51:26 +0200

    Update UtenteDAOPostgresImpl.java

--- src/main/java/implementazioneDao/UtenteDAOPostgresImpl.java
+++ src/main/java/implementazioneDao/UtenteDAOPostgresImpl.java
@@ -12,49 +12,48 @@ import java.sql.SQLException;
 public class UtenteDAOPostgresImpl implements UtenteDAO {
 
     @Override
-    public UTENTE verificaLogin(String username, String password) {
-        UTENTE utenteLoggato = null;
+    public Utente verificaLogin(String username, String password) {
 
+        Utente utenteLoggato = null;
         String query = "SELECT * FROM Utenti WHERE username = ? AND password = ?";
 
-        try {
-            Connection connection = DBConnection.getInstance().getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
+        // La connessione viene recuperata dal Singleton
+        Connection connection = DBConnection.getInstance().getConnection();
+
+        // TRY-WITH-RESOURCES: garantisce la chiusura automatica dello Statement
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+
             statement.setString(1, username);
             statement.setString(2, password);
 
-            ResultSet resultSet = statement.executeQuery();
+            // TRY-WITH-RESOURCES annidato: garantisce la chiusura del ResultSet
+            try (ResultSet resultSet = statement.executeQuery()) {
 
-            if (resultSet.next()) {
-                String nome = resultSet.getString("nome");
-                String cognome = resultSet.getString("cognome");
-                String ruolo = resultSet.getString("ruolo");
+                if (resultSet.next()) {
+                    String nome = resultSet.getString("nome");
+                    String cognome = resultSet.getString("cognome");
+                    String ruolo = resultSet.getString("ruolo");
 
-                if (ruolo.equalsIgnoreCase("Studente")) {
-                    String matricola = resultSet.getString("matricola");
-                    String corsoStudi = resultSet.getString("corso_studi");
-                    utenteLoggato = new STUDENTE(username, password, nome, cognome, matricola, corsoStudi);
+                    if (ruolo.equalsIgnoreCase("Studente")) {
+                        String matricola = resultSet.getString("matricola");
+                        String corsoStudi = resultSet.getString("corso_studi");
+                        utenteLoggato = new STUDENTE(username, password, nome, cognome, matricola, corsoStudi);
 
-                } else if (ruolo.equalsIgnoreCase("Docente")) {
-                    String codiceDocente = resultSet.getString("codice_docente");
+                    } else if (ruolo.equalsIgnoreCase("Docente")) {
+                        String codiceDocente = resultSet.getString("codice_docente");
+                        // Ora istanziamo la classe passando tutti i parametri
+                        utenteLoggato = new Utente(username, password, nome, cognome, codiceDocente);
 
-                    // --- LA MODIFICA È QUI ---
-                    // Ora istanziamo la classe concreta DOCENTE passandole tutti i parametri,
-                    // esattamente come hai fatto per lo Studente e il Coordinatore.
-                    utenteLoggato = new UTENTE(username, password, nome, cognome, codiceDocente);
-
-                } else if (ruolo.equalsIgnoreCase("Coordinatore")) {
-                    String idCoordinatore = resultSet.getString("id_coordinatore");
-                    utenteLoggato = new COORDINATORE(username, password, nome, cognome, idCoordinatore);
+                    } else if (ruolo.equalsIgnoreCase("Coordinatore")) {
+                        String idCoordinatore = resultSet.getString("id_coordinatore");
+                        utenteLoggato = new COORDINATORE(username, password, nome, cognome, idCoordinatore);
+                    }
                 }
-            }
-
-            resultSet.close();
-            statement.close();
+            } // Qui il ResultSet viene chiuso automaticamente
 
         } catch (SQLException e) {
-            System.err.println("Errore: " + e.getMessage());
-        }
+            System.err.println("Errore di connessione o esecuzione query: " + e.getMessage());
+        } // Qui il PreparedStatement viene chiuso automaticamente
 
         return utenteLoggato;
     }
