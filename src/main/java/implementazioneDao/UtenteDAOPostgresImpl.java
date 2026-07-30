348bccb72edad7190cd34cf83d04809e188df51e
agostinolandolfo21 <agostinolandolfo21@gmail.com>
2026-07-30 16:34:09 +0200

    Update UtenteDAOPostgresImpl.java

--- src/main/java/implementazioneDao/UtenteDAOPostgresImpl.java
+++ src/main/java/implementazioneDao/UtenteDAOPostgresImpl.java
@@ -2,31 +2,30 @@ package implementazionepostgresdao;
 
 import dao.UtenteDAO;
 import database.DBConnection;
-import model.*;
+import model.UTENTE;
+import model.STUDENTE;
+import model.COORDINATORE;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UtenteDAOPostgresImpl implements UtenteDAO {
+public class UTENTEDAOPostgresImpl implements UtenteDAO {
 
     @Override
-    public Utente verificaLogin(String username, String password) {
+    public UTENTE verificaLogin(String username, String password) {
 
-        Utente utenteLoggato = null;
+        UTENTE utenteLoggato = null;
         String query = "SELECT * FROM Utenti WHERE username = ? AND password = ?";
 
-        // La connessione viene recuperata dal Singleton
         Connection connection = DBConnection.getInstance().getConnection();
 
-        // TRY-WITH-RESOURCES: garantisce la chiusura automatica dello Statement
         try (PreparedStatement statement = connection.prepareStatement(query)) {
 
             statement.setString(1, username);
             statement.setString(2, password);
 
-            // TRY-WITH-RESOURCES annidato: garantisce la chiusura del ResultSet
             try (ResultSet resultSet = statement.executeQuery()) {
 
                 if (resultSet.next()) {
@@ -41,19 +40,18 @@ public class UtenteDAOPostgresImpl implements UtenteDAO {
 
                     } else if (ruolo.equalsIgnoreCase("Docente")) {
                         String codiceDocente = resultSet.getString("codice_docente");
-                        // Ora istanziamo la classe passando tutti i parametri
-                        utenteLoggato = new Utente(username, password, nome, cognome, codiceDocente);
+                        utenteLoggato = new UTENTE(username, password, nome, cognome, codiceDocente);
 
                     } else if (ruolo.equalsIgnoreCase("Coordinatore")) {
                         String idCoordinatore = resultSet.getString("id_coordinatore");
                         utenteLoggato = new COORDINATORE(username, password, nome, cognome, idCoordinatore);
                     }
                 }
-            } // Qui il ResultSet viene chiuso automaticamente
+            }
 
         } catch (SQLException e) {
             System.err.println("Errore di connessione o esecuzione query: " + e.getMessage());
-        } // Qui il PreparedStatement viene chiuso automaticamente
+        }
 
         return utenteLoggato;
     }
