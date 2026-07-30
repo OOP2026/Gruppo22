b66b755023003dffcbf2be6e95bf46e923859dc6
agostinolandolfo21 <agostinolandolfo21@gmail.com>
2026-07-30 15:16:36 +0200

    Update TesiDAOPostgresImpl.java

--- src/main/java/implementazioneDao/TesiDAOPostgresImpl.java
+++ src/main/java/implementazioneDao/TesiDAOPostgresImpl.java
@@ -3,6 +3,7 @@ package implementazionepostgresdao;
 import dao.TesiDAO;
 import database.DBConnection;
 import model.TESI;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,11 +22,11 @@ public class TesiDAOPostgresImpl implements TesiDAO {
             while (rs.next()) {
                 TESI t = new TESI();
                 t.setTitolo(rs.getString("titolo"));
-                // Mappatura degli altri campi...
+                // In futuro: mappatura degli altri campi qui...
                 lista.add(t);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.err.println("Errore durante il recupero delle tesi: " + e.getMessage());
         }
         return lista;
     }
@@ -33,33 +34,47 @@ public class TesiDAOPostgresImpl implements TesiDAO {
     @Override
     public boolean save(TESI tesi) {
         String query = "INSERT INTO tesi (titolo, stato) VALUES (?, ?)";
+
         try (Connection conn = DBConnection.getInstance().getConnection();
              PreparedStatement pstmt = conn.prepareStatement(query)) {
 
             pstmt.setString(1, tesi.getTitolo());
-            pstmt.setString(2, tesi.getStato().toString()); // Convertiamo l'enum[cite: 6]
-            pstmt.executeUpdate();
+            pstmt.setString(2, tesi.getStato().toString()); // Conversione dell'enum
+
+            int affectedRows = pstmt.executeUpdate();
+            return affectedRows > 0; // Ritorna true solo se l'inserimento ha avuto successo
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.err.println("Errore durante il salvataggio della tesi: " + e.getMessage());
+            return false;
         }
-        return false;
     }
 
     @Override
-    public void update(TESI tesi) { /* Logica di aggiornamento */ }
-
-
-    // Nell'implementazione:
-    boolean inserisciTesi() {
-        return inserisciTesi();
+    public void update(TESI tesi) {
+        /* Logica di aggiornamento da implementare in futuro */
     }
 
-    // Nell'implementazione:
+    // N.B.: Il metodo ricorsivo errato 'boolean inserisciTesi()' è stato rimosso per design.
+
     @Override
     public boolean inserisciTesi(TESI tesi) {
         String query = "INSERT INTO tesi (titolo, descrizione, codice_docente, tipologia) VALUES (?, ?, ?, ?)";
-        // ... qui metti il tuo try-catch con PreparedStatement e executeUpdate() ...
-        return true; // Ritorna true se l'inserimento ha successo
+
+        try (Connection conn = DBConnection.getInstance().getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+
+            pstmt.setString(1, tesi.getTitolo());
+            pstmt.setString(2, tesi.getDescrizione());
+            pstmt.setString(3, tesi.getCodiceDocente());
+            pstmt.setString(4, tesi.getTipologia());
+
+            int affectedRows = pstmt.executeUpdate();
+            return affectedRows > 0; // Ritorna true se l'inserimento è andato a buon fine
+
+        } catch (SQLException e) {
+            System.err.println("Errore durante l'inserimento della tesi: " + e.getMessage());
+            return false;
+        }
     }
 }
