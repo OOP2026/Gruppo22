b8187d7bdd93bb90b2629b0c2a9536d0ed46f2a8
agostinolandolfo21 <agostinolandolfo21@gmail.com>
2026-08-01 13:29:22 +0200

    Update TirocinioDAOPostgresImpl.java

--- src/main/java/implementazioneDao/TirocinioDAOPostgresImpl.java
+++ src/main/java/implementazioneDao/TirocinioDAOPostgresImpl.java
@@ -2,20 +2,26 @@ package implementazionedao;
 
 import dao.TirocinioDAO;
 import database.DBConnection;
-import model.TIPO_ESTERNO;
-import model.TIPO_INTERNO;
+import model.Tipo_Esterno;
+import model.Tipo_Interno;
 import model.Tirocinio;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TirocinioDAOPostgresImpl implements TirocinioDAO {
+
+    // Inizializzazione del Logger di classe
+    private static final Logger LOGGER = Logger.getLogger(TirocinioDAOPostgresImpl.class.getName());
 
     @Override
     public List<Tirocinio> findAll() {
         List<Tirocinio> tirocini = new ArrayList<>();
 
-        // La query UNION che fonde le due tabelle!
+        // La query UNION che fonde le due tabelle
         String query =
                 "SELECT id_intero AS id, 'INTERNO' AS tipologia, laboratorio AS ente, 'Sede Universitaria' AS indirizzo " +
                         "FROM tipo_interno " +
@@ -29,28 +35,30 @@ public class TirocinioDAOPostgresImpl implements TirocinioDAO {
 
             while (rs.next()) {
                 Tirocinio t = new Tirocinio();
-                // Usa i setter che hai definito nella classe TIROCINIO
+
+                // Binding dei risultati sull'oggetto Tirocinio
                 t.setIdTirocinio(rs.getInt("id"));
                 t.setTipologia(rs.getString("tipologia"));
                 t.setAziendaEnte(rs.getString("ente"));
-                // Se nella classe TIROCINIO non hai l'indirizzo, puoi omettere questa riga
-                // t.setIndirizzo(rs.getString("indirizzo"));
 
                 tirocini.add(t);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            // Sostituzione di e.printStackTrace() con il Logger ufficiale
+            LOGGER.log(Level.SEVERE, "Errore durante l'estrazione della lista dei tirocini", e);
         }
         return tirocini;
     }
 
     @Override
-    public void saveInterno(TIPO_INTERNO t) {
-
+    // Metodo aggiornato con la nuova classe Tipo_Interno
+    public void saveInterno(Tipo_Interno t) {
+        // Da implementare
     }
 
     @Override
-    public void saveEsterno(TIPO_ESTERNO t) {
-
+    // Metodo aggiornato con la nuova classe Tipo_Esterno
+    public void saveEsterno(Tipo_Esterno t) {
+        // Da implementare
     }
 }
