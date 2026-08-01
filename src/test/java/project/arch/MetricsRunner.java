800e086a8ec8c52a92b57393104900ef5d45f512
agostinolandolfo21 <agostinolandolfo21@gmail.com>
2026-08-01 17:41:02 +0200

    Update MetricsRunner.java

--- src/test/java/project/arch/MetricsRunner.java
+++ src/test/java/project/arch/MetricsRunner.java
@@ -13,14 +13,19 @@ import java.util.Set;
 
 public class MetricsRunner {
     public static void main(String[] args) {
-        JavaClasses classes = new ClassFileImporter().withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS).importPackages("project");
+        JavaClasses classes = new ClassFileImporter()
+                .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+                .importPackages("project");
+
         Set<JavaPackage> packages = classes.getPackage("project").getSubpackages();
 
         MetricsComponents<JavaClass> components = MetricsComponents.fromPackages(packages);
 
         ComponentDependencyMetrics metrics = ArchitectureMetrics.componentDependencyMetrics(components);
 
-        components.stream().forEach(component -> {
+        // --- LA MODIFICA È QUI ---
+        // Rimozione di .stream() per un'iterazione diretta e ottimizzata
+        components.forEach(component -> {
             System.out.println("Component: " + component.getIdentifier());
             System.out.println("Ce: " + metrics.getEfferentCoupling(component.getIdentifier()));
             System.out.println("Ca: " + metrics.getAfferentCoupling(component.getIdentifier()));
