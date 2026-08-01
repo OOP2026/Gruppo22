14e97b1e1867946109c9f07a932470b7bcd4c972
agostinolandolfo21 <agostinolandolfo21@gmail.com>
2026-08-01 16:14:25 +0200

    Update Tipo_Esterno.java

--- src/main/java/model/Tipo_Esterno.java
+++ src/main/java/model/Tipo_Esterno.java
@@ -1,14 +1,28 @@
 package model;
 
-// Implementazione Esterno[cite: 5]
+// Implementazione Esterno
 public class Tipo_Esterno extends Tirocinio {
-    public static final String titoloArgomento = "";
+
+    // --- LA MODIFICA È QUI ---
+    // Le costanti (static final) in Java devono essere scritte tutte in MAIUSCOLO
+    public static final String titoloargomento = "";
+
     private String referenteAziendale;
-    public Tipo_Esterno() { super(); }
+
+    public Tipo_Esterno() {
+        super();
+    }
+
     public Tipo_Esterno(String titoloArgomento, String referenteAziendale) {
         super();
         this.referenteAziendale = referenteAziendale;
     }
-    public String getReferenteAziendale() { return referenteAziendale; }
-    public void setReferenteAziendale(String referenteAziendale) { this.referenteAziendale = referenteAziendale; }
+
+    public String getReferenteAziendale() {
+        return referenteAziendale;
+    }
+
+    public void setReferenteAziendale(String referenteAziendale) {
+        this.referenteAziendale = referenteAziendale;
+    }
 }
