a80174799e6286a3763033abc264100bd3699312
agostinolandolfo21 <agostinolandolfo21@gmail.com>
2026-08-01 17:11:09 +0200

    Update SedutaDiLaurea.java

--- src/main/java/model/SedutaDiLaurea.java
+++ src/main/java/model/SedutaDiLaurea.java
@@ -1,51 +1,69 @@
 package model;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class SedutaDiLaurea {
+
     private LocalDate data;
-    private LocalTime ora_inizio;
-    private LocalTime ora_fine;
-    private String luogo;
+
+    // Rinominate le variabili rimuovendo l'underscore (camelCase)
+    private LocalTime oraInizio;
+    private LocalTime oraFine;
     private String ora;
+    private String luogo;
     private String aula;
 
     public SedutaDiLaurea() {}
 
-    public SedutaDiLaurea(LocalDate data, LocalTime ora_inizio, LocalTime ora_fine, String luogo) {
+    public SedutaDiLaurea(LocalDate data, LocalTime oraInizio, LocalTime oraFine, String luogo) {
         this.data = data;
-        this.ora_inizio = ora_inizio;
-        this.ora_fine = ora_fine;
+        this.oraInizio = oraInizio;
+        this.oraFine = oraFine;
         this.luogo = luogo;
     }
 
     public LocalDate getData() {
         return data;
     }
-    public LocalTime getOra_inizio() {
-        return ora_inizio;
+
+    // Rinominato in getOraInizio
+    public LocalTime getOraInizio() {
+        return oraInizio;
     }
 
-    public LocalTime getOra_fine() {
-        return ora_fine;
+    // Rinominato in getOraFine
+    public LocalTime getOraFine() {
+        return oraFine;
     }
 
     public String getLuogo() {
         return luogo;
     }
 
-    // Assicurati di avere anche i rispettivi setter se ti servono:
-    public void setData(LocalDate data) { this.data = data; }
-    public void setOra_inizio(LocalTime ora_inizio) { this.ora_inizio = ora_inizio; }
-    public void setOra_fine(LocalTime ora_fine) { this.ora_fine = ora_fine; }
-    public void setLuogo(String luogo) { this.luogo = luogo; }
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    // Rinominato in setOraInizio
+    public void setOraInizio(LocalTime oraInizio) {
+        this.oraInizio = oraInizio;
+    }
+
+    // Rinominato in setOraFine
+    public void setOraFine(LocalTime oraFine) {
+        this.oraFine = oraFine;
+    }
+
+    public void setLuogo(String luogo) {
+        this.luogo = luogo;
+    }
 
     public void setOra(String ora) {
-        this.ora = ora;
     }
 
     public LocalTime getOra() {
-        return ora_inizio;
+        return oraInizio;
     }
 
     public void setAula(String aula) {
@@ -55,5 +73,4 @@ public class SedutaDiLaurea {
     public String getAula() {
         return aula;
     }
-    // Getter e Setter[cite: 8]
 }
