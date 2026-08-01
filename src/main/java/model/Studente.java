e4c2243f05ebb68f6a4ce86305fa6f7ad89ab8b6
agostinolandolfo21 <agostinolandolfo21@gmail.com>
2026-08-01 16:22:04 +0200

    Update Studente.java

--- src/main/java/model/Studente.java
+++ src/main/java/model/Studente.java
@@ -1,12 +1,15 @@
 package model;
 
-public class Studente extends Utente { // UTENTE dovrebbe contenere username, password, nome, cognome[cite: 2]
+public class Studente extends Utente {
+
     private String matricola;
     private String corsoStudi;
 
     // Costruttore principale
     public Studente(String username, String password, String nome, String cognome, String matricola, String corsoStudi) {
-        super(); // Passa i dati alla classe UTENTE
+        // --- MODIFICA ARCHITETTURALE ---
+        // Passiamo i parametri ricevuti direttamente al costruttore della classe padre (Utente)
+        super(username, password, nome, cognome);
         this.matricola = matricola;
         this.corsoStudi = corsoStudi;
     }
@@ -17,16 +20,23 @@ public class Studente extends Utente { // UTENTE dovrebbe contenere username, pa
     }
 
     // --- GETTER e SETTER ---
-    public String getMatricola() { return matricola; }
-    public void setMatricola(String matricola) { this.matricola = matricola; }
 
-    public String getCorsoStudi() {
-        return corsoStudi; }
-    public void setCorsoStudi(String corsoStudi) {
-        this.corsoStudi = corsoStudi; }
-
-    public void setNome(String nome) {
+    public String getMatricola() {
+        return matricola;
     }
 
-    // NOTA: Non serve ridefinire getNome() o setNome() se li hai in UTENTE[cite: 2]
+    public void setMatricola(String matricola) {
+        this.matricola = matricola;
+    }
+
+    public String getCorsoStudi() {
+        return corsoStudi;
+    }
+
+    public void setCorsoStudi(String corsoStudi) {
+        this.corsoStudi = corsoStudi;
+    }
+
+    // Il metodo vuoto setNome() è stato rimosso per sfruttare correttamente l'ereditarietà.
+    // I metodi per nome, cognome, username e password vengono ereditati in automatico da Utente.
 }
