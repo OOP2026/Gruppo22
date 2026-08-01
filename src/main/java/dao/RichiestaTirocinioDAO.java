4fba41f7c27a6bb01136b2e91d3a34f9c4092b6f
agostinolandolfo21 <agostinolandolfo21@gmail.com>
2026-08-01 17:04:16 +0200

    Update RichiestaTirocinioDAO.java

--- src/main/java/dao/RichiestaTirocinioDAO.java
+++ src/main/java/dao/RichiestaTirocinioDAO.java
@@ -1,14 +1,9 @@
 package dao;
 
-import database.DBConnection;
 import model.RichiestaTirocinio;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 public interface RichiestaTirocinioDAO {
-    void save(RichiestaTirocinio r);
-}
 
+    void save(RichiestaTirocinio r);
+
+}
